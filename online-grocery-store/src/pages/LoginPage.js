7c257f5d736c5d82588e544735f08a09a9f400d2
balaji-18-05 <balaji9750452687@gmail.com>
2025-02-18 01:46:11 +0530

    login page started

--- online-grocery-store/src/pages/LoginPage.js
+++ online-grocery-store/src/pages/LoginPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import '../login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bgImage from 'bgimage.jpg';
 export default function LoginPage() {
   return (
-    <center>
-        <div>LoginPage</div>
-    </center>
+    <>
+        <div className='login-container'>
+          <label>Username : </label><input type='text' placeholder='Enter your name...'></input>
+          <label>Email : </label><input type='mail' placeholder='Enter your mail id'></input>
+          <label>Password : </label><input type='password' placeholder='Enter your password id'></input>
+          <button className='btn btn-outline-primary'>submit</button>
+        </div>
+          <div style={{ 
+          backgroundImage: `url(${bgImage})`, 
+          backgroundSize: 'cover', 
+          height: '100vh' 
+          }}></div>
+    </>
   )
 }
